2d28ab32ccbec16724343b4ca89686c06616c1ac
luciano588 <luciano588.alm@gmail.com>
2020-12-19 18:13:53 +0000

    12/19-1p

--- src/js/component/card.js
+++ src/js/component/card.js
@@ -1,30 +1,25 @@
 import React from "react";
-import PropTypes from "prop-types"
+import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
-
 export const Card = props => {
-    const {store, actions} = useContext(context);\
-    return (
-        <div className="card" style={{ width: "18rem"}} >
-
-    <img src={props.img} className="card-img-top"  alt=".."/>
-    <div className="card-body">
-    <h5 className="card-title">{props.title}}</h5>
-    <Link to={btnUrl} className="btn btn-primary">{props.btnTitle}}</Link>
-    </div>
-    </div>
-    );
-
+	return (
+		<div className="card" style={{ width: "18rem" }}>
+			<img src={props.img} className="card-img-top" alt="" />
+			<div className="card-body">
+				<h5 className="card-title">{props.title}</h5>
+				<Link to={props.btnUrl} className="btn btn-primary">
+					{props.btnTitle}
+				</Link>
+			</div>
+		</div>
+	);
 };
 Card.propTypes = {
-    title: PropTypes.string,
-    btnUrl: PropTypes.string,
-    btnTitle: PropTypes.string,
-    img: PropTypes.string,
-    
-
-
+	title: PropTypes.string,
+	btnUrl: PropTypes.string,
+	btnTitle: PropTypes.string,
+	img: PropTypes.string
 };
 
 // export const card = () =>{
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,28 +1,31 @@
 import React, { useContext } from "react";
 import "../../styles/home.scss";
 import { Context } from "../store/appContext";
-import { Card } from "../store/card";
+import { Card } from "../component/card";
 
 export const Home = () => {
-    const {store, actions} = useContext(context);
-    
-    const buildRow = (data,type) => {
-        return data.map((item, index)=>{
-            return <Card 
-                    img={"https://via.placeholder.com/300"}
-                    key={index}
-                    title={item.name}
-                    btnURL={`/${type}/${index}`} 
-                    btnTitle="View more" />
-        );
-    });
+	const { store, actions } = useContext(Context);
 
-    return (
-	<div className="text-center mt-5">
-		<h1>Star Wars Blog!</h1>
-		<div className="resource-row">{buildRow(store.planets, "planets")}</div>
-		<div className="row">{buildRow(store.people, "people")}</div>
-        <div className="row">{buildRow(store.vehicles, "vehicles")}</div>     
-    </div>
-    );
+	const buildRow = (data, type) => {
+		return data.map((item, index) => {
+			return (
+				<Card
+					img={"https://via.placeholder.com/300"}
+					key={index}
+					title={item.name}
+					btnURL={`/${type}/${index}`}
+					btnTitle="View more"
+				/>
+			);
+		});
+	};
+
+	return (
+		<div className="text-center mt-5">
+			<h1>Star Wars Blog!</h1>
+			<div className="resource-row">{buildRow(store.planets, "planets")}</div>
+			<div className="resource-row">{buildRow(store.people, "people")}</div>
+			<div className="resource-row">{buildRow(store.vehicles, "vehicles")}</div>
+		</div>
+	);
 };
